dc997902b8c4f074228fa17325b7d594d7ecee9e
SamiranDas2004 <samiran4209@gmail.com>
2026-01-22 23:35:10 +0530

    push

--- components/ui/text-hover-effect.tsx
+++ components/ui/text-hover-effect.tsx
@@ -32,7 +32,7 @@ export const TextHoverEffect = ({
       ref={svgRef}
       width="100%"
       height="100%"
-      viewBox="0 0 1200 100"
+      viewBox="0 0 1000 100"
       preserveAspectRatio="xMidYMid meet"
       xmlns="http://www.w3.org/2000/svg"
       onMouseEnter={() => setHovered(true)}
